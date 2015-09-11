f1cdc7979e8a3843bc399e7f000b05b781053ec4
Marcus Bannerman <m.bannerman@gmail.com>
2015-09-11 09:52:24 +0100

    Improved formatting of images and equations, added support for display equations

--- BlackboardQuiz.py
+++ BlackboardQuiz.py
@@ -4,11 +4,11 @@ from lxml import etree
 import time
 import zipfile
 
-import pylab
+import matplotlib.pyplot as plt
 from cStringIO import StringIO
 
 class Quiz:
-    def __init__(self, quizname, description_text="Created by BlackboardQuiz!"):
+    def __init__(self, quizname, description_text="Created by BlackboardQuiz!", useLaTeX=False):
         """Initialises a quiz
         """
         self.quizname = quizname
@@ -38,6 +38,11 @@ class Quiz:
         resources = etree.SubElement(self.manifest, 'resources')
         resource = etree.SubElement(resources, 'resource', {'baseurl':"res00001", 'file':"res00001.dat", 'identifier':"res00001", 'type':"assessment/x-bb-pool"})
 
+        if useLaTeX:
+            #Use latex (not mathtex) for better but slower results
+            from matplotlib import rc
+            rc('text', usetex=True)
+
     def close(self):
         self.zf.writestr('imsmanifest.xml', '<?xml version="1.0" encoding="utf-8"?>\n'+etree.tostring(self.manifest, pretty_print=True))
         self.zf.writestr('res00001.dat', '<?xml version="1.0" encoding="utf-8"?>\n'+etree.tostring(self.pool, pretty_print=True))
@@ -58,12 +63,12 @@ class Quiz:
         return str(self.resource_counter)+'_1'
 
 
-    def render_latex(self, formula, fontsize=6, dpi=300, format_='png'):
+    def render_latex(self, formula, fontsize=12, dpi=120, format_='png'):
         """Renders LaTeX expression into an image data.
         """
-        fig = pylab.figure()
+        fig = plt.figure()
         text = fig.text(0, 0, u'${}$'.format(formula), fontsize=fontsize)
-        #Fake render to force pylab to determine the actual size of the text
+        #Fake render to force matplotlib to determine the actual size of the text
         buffer_ = StringIO()
         fig.savefig(buffer_, dpi=dpi, format=format_, transparent=True)
 
@@ -82,31 +87,43 @@ class Quiz:
         fig.savefig(buffer_, dpi=dpi, format=format_, transparent=True)
         return buffer_.getvalue()
 
-    def embed_image(self, filename, img_data=None):
+    def embed_image(self, filename, img_data=None, attrib={'style':'display:block; margin-left:auto; margin-right:auto;'}):
         """Embeds image data or a file, and returns an img tag.
         """
         if img_data == None:
             with open(filename, mode='rb') as file:
                 img_data = file.read()
+
+        output = '<img src="@X@EmbeddedFile.requestUrlStub@X@bbcswebdav/xid-'+self.addfile(filename, img_data)+'"'
+        for key, value in attrib.items():
+            output += " "+key+'="'+value+'"'
+        output += '>'
+        return output
         
-        return '<img src="@X@EmbeddedFile.requestUrlStub@X@bbcswebdav/xid-'+self.addfile(filename, img_data)+'">'
-        
-    def embed_latex(self, formula):
+    def embed_latex(self, formula, attrib={}):
         """Renders a LaTeX formula to an image, embeds the image in the quiz
         and returns a img tag which can be used in the text of a
         question or answer.
         """
         name = "eq"+str(self.equation_counter)+".png"
         self.equation_counter += 1
-        return self.embed_image(name, self.render_latex(formula))
+        return self.embed_image(name, self.render_latex(formula), attrib=attrib)
 
     def process_latex(self, in_string):
         """Scan a string for LaTeX equations and process them.
         """
+        #First, process the display equations
+        in_string = in_string.split('$$')
+        for i in range(1, len(in_string), 2):
+            in_string[i] = self.embed_latex(in_string[i], attrib={'style':'display:block;margin-left:auto;margin-right:auto;'})
+        in_string = ''.join(in_string)
+
         in_string = in_string.split('$')
         for i in range(1, len(in_string), 2):
-            in_string[i] = self.embed_latex(in_string[i])
-        return ''.join(in_string)
+            in_string[i] = self.embed_latex(in_string[i], attrib={'style':'vertical-align:middle'})
+        in_string = ''.join(in_string)
+
+        return in_string
 
     def addDates(self, element):
         """Helper function to add the DATES section
@@ -138,6 +155,6 @@ class Quiz:
             etree.SubElement(answer, 'TEXT').text = self.process_latex(text)
 
         gradable = etree.SubElement(q_node, 'GRADABLE')
-        etree.SubElement(gradable, 'FEEDBACK_WHEN_CORRECT').text = positive_feedback
-        etree.SubElement(gradable, 'FEEDBACK_WHEN_INCORRECT').text = negative_feedback
+        etree.SubElement(gradable, 'FEEDBACK_WHEN_CORRECT').text = self.process_latex(positive_feedback)
+        etree.SubElement(gradable, 'FEEDBACK_WHEN_INCORRECT').text = self.process_latex(negative_feedback)
         etree.SubElement(gradable, 'CORRECTANSWER', {'answer_id':question_id+'_a'+str(correct)})
