225caebd0bc134eb9f3b35265d8d3094a4a4cd68
Marcus Bannerman <m.bannerman@gmail.com>
2015-09-13 22:46:11 +0100

    Broken commit

--- BlackboardQuiz.py
+++ BlackboardQuiz.py
@@ -1,52 +1,118 @@
 #!/usr/bin/env python
 
 from lxml import etree
+import lxml.html as html
 import time
 import zipfile
+import re
 
 import matplotlib.pyplot as plt
 from cStringIO import StringIO
 
-class Quiz:
-    def __init__(self, quizname, description_text="Created by BlackboardQuiz!", useLaTeX=False):
+class Pool:
+    def __init__(self, pool_name, package, description_text="Created by BlackboardQuiz!"):
         """Initialises a quiz
         """
-        self.quizname = quizname
+        self.package = package
+        self.pool_name = pool_name
+
+        #Create the question datafile
+        self.pool = etree.Element("POOL")
+        etree.SubElement(self.pool, 'COURSEID', {'value':self.package.courseID})
+        etree.SubElement(self.pool, 'TITLE', {'value':self.pool_name})
+        description = etree.SubElement(self.pool, 'DESCRIPTION')
+        etree.SubElement(description, 'TEXT').text = description_text
+        self.package.addDates(self.pool)
+        self.questionlist = etree.SubElement(self.pool, 'QUESTIONLIST')
+        self.question_counter = 0
+
+        #Create the manifest file
+
+        self.htmlfile = "<html><head><style>li.correct, li.incorrect{list-style-type:none;} li.correct:before{content:'\\2713\\0020'}\nli.incorrect:before{content:'\\2718\\0020'}</style></head><body><p>Questions<ul>"
+
+    def __enter__(self):
+        return self
+        
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def close(self):
+        self.package.zf.writestr(self.pool_name+'_preview.html', self.htmlfile)
+        self.package.embed_resource(self.pool_name, "assessment/x-bb-pool", '<?xml version="1.0" encoding="utf-8"?>\n'+etree.tostring(self.pool, pretty_print=True))
+
+    def addQuestion(self, text, answers, correct, positive_feedback="Good work", negative_feedback="That's not correct"):
+        self.question_counter += 1 
+        question_id = 'q'+str(self.question_counter)
+        #Add the question to the list of questions
+        etree.SubElement(self.questionlist, 'QUESTION', {'id':question_id, 'class':'QUESTION_MULTIPLECHOICE'})
+        #Add the actual question node
+        q_node = etree.SubElement(self.pool, 'QUESTION_MULTIPLECHOICE', {'id':question_id})
+        self.package.addDates(q_node)
+        body = etree.SubElement(q_node, 'BODY')
+        bb_question_text, html_question_text = self.package.process_string(text)
+        self.htmlfile += '<li>'+html_question_text+'<ul>'
+        etree.SubElement(body, 'TEXT').text = bb_question_text
+        flags = etree.SubElement(body, 'FLAGS', {'value':'true'})
+        etree.SubElement(flags, 'ISHTML', {'value':'true'})
+        etree.SubElement(flags, 'ISNEWLINELITERAL')
+        
+        a_count = 0
+        for text in answers:
+            a_count += 1
+            a_id = question_id+'_a'+str(a_count)
+            answer = etree.SubElement(q_node, 'ANSWER', {'id':a_id, 'position':str(a_count)})
+            self.package.addDates(answer)
+            bb_answer_text, html_answer_text = self.package.process_string(text)
+            etree.SubElement(answer, 'TEXT').text = bb_answer_text
+
+            classname="incorrect"
+            if (a_count == correct):
+                classname="correct"
+
+            self.htmlfile += '<li class="'+classname+'">'+html_answer_text+'</li>'
+
+        gradable = etree.SubElement(q_node, 'GRADABLE')
+        bb_pos_feedback_text, html_pos_feedback_text = self.package.process_string(positive_feedback)
+        bb_neg_feedback_text, html_neg_feedback_text = self.package.process_string(negative_feedback)
+        etree.SubElement(gradable, 'FEEDBACK_WHEN_CORRECT').text = bb_pos_feedback_text
+        etree.SubElement(gradable, 'FEEDBACK_WHEN_INCORRECT').text = bb_neg_feedback_text
+        etree.SubElement(gradable, 'CORRECTANSWER', {'answer_id':question_id+'_a'+str(correct)})
+
+        self.htmlfile += '</ul>'
+        self.htmlfile += '<div>+:'+html_pos_feedback_text+'</div>'
+        self.htmlfile += '<div>-:'+html_neg_feedback_text+'</div>'
+        self.htmlfile += '</li>'
+
+class Package:
+    def __init__(self, courseID="IMPORT", useLaTeX=False):
+        """Initialises a Blackboard package
+        """
+        self.courseID = courseID
+        self.embedded_files = {}
         try:
             import zlib
             compression = zipfile.ZIP_DEFLATED
         except:
             compression = zipfile.ZIP_STORED
-        self.zf = zipfile.ZipFile(self.quizname+'.zip', mode='w', compression=compression)
-        self.resource_counter = 1000000
+        self.zf = zipfile.ZipFile(self.courseID+'.zip', mode='w', compression=compression)
+        self.next_xid = 1000000
         self.equation_counter = 0
-
-        #Create the question datafile
-        self.pool = etree.Element("POOL")
-        etree.SubElement(self.pool, 'COURSEID', {'value':'IMPORT'})
-        etree.SubElement(self.pool, 'TITLE', {'value':self.quizname})
-        description = etree.SubElement(self.pool, 'DESCRIPTION')
-        etree.SubElement(description, 'TEXT').text = description_text
-        self.addDates(self.pool)
-        self.questionlist = etree.SubElement(self.pool, 'QUESTIONLIST')
-        self.question_counter = 0
+        self.resource_counter = 1
 
         #Create the manifest file
         self.manifest = etree.Element("manifest", {'identifier':'man00001'})
         organisation = etree.SubElement(self.manifest, "organization", {'default':'toc00001'})
         etree.SubElement(organisation, 'tableofcontents', {'identifier':'toc00001'})
-        resources = etree.SubElement(self.manifest, 'resources')
-        resource = etree.SubElement(resources, 'resource', {'baseurl':"res00001", 'file':"res00001.dat", 'identifier':"res00001", 'type':"assessment/x-bb-pool"})
+        self.resources = etree.SubElement(self.manifest, 'resources')
 
         self.useLaTeX = useLaTeX
-        if useLaTeX:
+        if self.useLaTeX:
             #Use latex (not mathtex) for better but slower results
             from matplotlib import rc
-            rc('text', usetex=True)
+            rc('text', usetex=True)            
 
     def close(self):
         self.zf.writestr('imsmanifest.xml', '<?xml version="1.0" encoding="utf-8"?>\n'+etree.tostring(self.manifest, pretty_print=True))
-        self.zf.writestr('res00001.dat', '<?xml version="1.0" encoding="utf-8"?>\n'+etree.tostring(self.pool, pretty_print=True))
         self.zf.close()
 
     def __enter__(self):
@@ -54,18 +120,27 @@ class Quiz:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
-        
-    def addfile(self, name, content):
-        """Adds a file (given a name and content) to the quiz and returns the unique id of the file
-        """
-        self.resource_counter += 1
+
+    def createPool(self, pool_name, description_text="Created by BlackboardQuiz!"):
+        return Pool(pool_name, self, description_text)
+
+
+    def embed_resource(self, name, type, content):
+        resource = etree.SubElement(self.resources, 'resource', {'baseurl':name, 'file':name+'.dat', 'identifier':name, 'type':type})
+        self.zf.writestr(name+'.dat', content)
+
+    def embed_file_data(self, name, content = None):
+        """Embeds a file (given a name and content) to the quiz and returns the
+        unique id of the file, and the path to the file in the zip
+        """                
+        self.next_xid += 1
         name = name.split('.')
-        self.zf.writestr('csfiles/home_dir/'+self.quizname+'/'+name[0]+'__xid-'+str(self.resource_counter)+'_1.'+name[1], content)
-        return str(self.resource_counter)+'_1'
+        path = 'csfiles/home_dir/'+self.courseID+'/'+name[0]+'__xid-'+str(self.next_xid)+'_1.'+name[1]
+        self.zf.writestr(path, content)
+        return str(self.next_xid)+'_1', path
 
-
-    def render_latex(self, formula, fontsize=12, dpi=120, format_='png'):
-        """Renders LaTeX expression into an image data.
+    def render_latex(self, formula, fontsize=12, dpi=150, format_='png'):
+        """Renders LaTeX expression to bitmap image data.
         """
         fig = plt.figure()
         text = fig.text(0, 0, u'${}$'.format(formula), fontsize=fontsize)
@@ -87,20 +162,39 @@ class Quiz:
         buffer_ = StringIO()
         fig.savefig(buffer_, dpi=dpi, format=format_, transparent=True)
         plt.close(fig)
+
+        #This gives a 22px em height
         return buffer_.getvalue()
 
-    def embed_image(self, filename, img_data=None, attrib={'style':'display:block; margin-left:auto; margin-right:auto;'}):
-        """Embeds image data or a file, and returns an img tag.
+    def embed_file(self, filename, file_data=None, attrib={}):
+        """Embeds a file, and returns an img tag for use in blackboard, and an equivalent for html.
         """
-        if img_data == None:
-            with open(filename, mode='rb') as file:
-                img_data = file.read()
+        #Check if it is a real file being embedded
+        if file_data == None:
+            #Check if this file has already been embedded
+            if filename in self.embedded_files:
+                #It has, return the already embedded data
+                return self.embedded_files[filename]
 
-        output = '<img src="@X@EmbeddedFile.requestUrlStub@X@bbcswebdav/xid-'+self.addfile(filename, img_data)+'"'
+            #It has not, load the data
+            with open(filename, mode='rb') as file:
+                file_data = file.read()
+            xid, path = self.embed_file_data(filename, file_data)
+            self.embedded_files[filename] = (xid, path)
+            return xid, path
+        else:
+            return self.embed_file_data(filename, file_data)
+
+    def embed_image(self, filename, img_data=None, attrib={}):
+        xid, path = self.embed_file(filename, img_data)
+        output_bb = '<img src="@X@EmbeddedFile.requestUrlStub@X@bbcswebdav/xid-'+xid+'"'
+        output_html = '<img src="'+path+'"'
         for key, value in attrib.items():
-            output += " "+key+'="'+value+'"'
-        output += '>'
-        return output
+            output_bb += " "+key+'="'+value+'"'
+            output_html += " "+key+'="'+value+'"'
+        output_bb += '>'
+        output_html += '>'
+        return output_bb, output_html
         
     def embed_latex(self, formula, attrib={}):
         """Renders a LaTeX formula to an image, embeds the image in the quiz
@@ -111,23 +205,45 @@ class Quiz:
         self.equation_counter += 1
         return self.embed_image(name, self.render_latex(formula), attrib=attrib)
 
-    def process_latex(self, in_string):
-        """Scan a string for LaTeX equations and process them.
+    def process_string(self, in_string):
+        """Scan a string for LaTeX equations, image tags, etc, and process them.
         """
-        #First, process the display equations
+        #Process img tags
+        pattern = re.compile(r"<img.*?>")
+
+        def img_src_processor(img_txt, html_mode):
+            img_tag = html.fragment_fromstring(img_txt)
+            xid, path = self.embed_file(img_tag.attrib['src'])
+            if html_mode:
+                img_tag.attrib['src'] = path
+            else:
+                img_tag.attrib['src'] = '@X@EmbeddedFile.requestUrlStub@X@bbcswebdav/xid-'+xid
+            return html.tostring(img_tag)
+
+        html_string = pattern.sub(lambda match : img_src_processor(match.group(0), True), in_string)
+        in_string = pattern.sub(lambda match : img_src_processor(match.group(0), False), in_string)
+        
+        #Process display LaTeX equations
         in_string = in_string.split('$$')
+        html_string = html_string.split('$$')
         for i in range(1, len(in_string), 2):
             if self.useLaTeX:
                 in_string[i] = (r'\displaystyle ')+in_string[i]
-            in_string[i] = self.embed_latex(in_string[i], attrib={'style':'display:block;margin-left:auto;margin-right:auto;'})
+            bb_img, html_img = self.embed_latex(in_string[i], attrib={'style':'display:block;margin-left:auto;margin-right:auto;'})
+            in_string[i] = bb_img
+            html_string[i] = html_img
         in_string = ''.join(in_string)
+        html_string = ''.join(html_string)
 
+        #Process inline LaTeX equations
         in_string = in_string.split('$')
+        html_string = html_string.split('$')
         for i in range(1, len(in_string), 2):
-            in_string[i] = self.embed_latex(in_string[i], attrib={'style':'vertical-align:middle'})
-        in_string = ''.join(in_string)
+            bb_img, html_img = self.embed_latex(in_string[i], attrib={'style':'vertical-align:bottom; height:1.1em;'})
+            in_string[i] = bb_img
+            html_string[i] = html_img
 
-        return in_string
+        return ''.join(in_string), ''.join(html_string)
 
     def addDates(self, element):
         """Helper function to add the DATES section
@@ -135,30 +251,3 @@ class Quiz:
         dates = etree.SubElement(element, 'DATES')
         etree.SubElement(dates, 'CREATED', {'value':time.strftime('%Y-%m-%d %H:%M:%SZ')})
         etree.SubElement(dates, 'UPDATED', {'value':time.strftime('%Y-%m-%d %H:%M:%SZ')})
-    
-    def addQuestion(self, text, answers, correct, positive_feedback="Good work", negative_feedback="That's not correct"):
-        self.question_counter += 1 
-        question_id = 'q'+str(self.question_counter)
-        #Add the question to the list of questions
-        etree.SubElement(self.questionlist, 'QUESTION', {'id':question_id, 'class':'QUESTION_MULTIPLECHOICE'})
-        #Add the actual question node
-        q_node = etree.SubElement(self.pool, 'QUESTION_MULTIPLECHOICE', {'id':question_id})
-        self.addDates(q_node)
-        body = etree.SubElement(q_node, 'BODY')
-        etree.SubElement(body, 'TEXT').text = self.process_latex(text)
-        flags = etree.SubElement(body, 'FLAGS', {'value':'true'})
-        etree.SubElement(flags, 'ISHTML', {'value':'true'})
-        etree.SubElement(flags, 'ISNEWLINELITERAL')
-        
-        a_count = 0
-        for text in answers:
-            a_count += 1
-            a_id = question_id+'_a'+str(a_count)
-            answer = etree.SubElement(q_node, 'ANSWER', {'id':a_id, 'position':str(a_count)})
-            self.addDates(answer)
-            etree.SubElement(answer, 'TEXT').text = self.process_latex(text)
-
-        gradable = etree.SubElement(q_node, 'GRADABLE')
-        etree.SubElement(gradable, 'FEEDBACK_WHEN_CORRECT').text = self.process_latex(positive_feedback)
-        etree.SubElement(gradable, 'FEEDBACK_WHEN_INCORRECT').text = self.process_latex(negative_feedback)
-        etree.SubElement(gradable, 'CORRECTANSWER', {'answer_id':question_id+'_a'+str(correct)})
--- python_example.py
+++ python_example.py
@@ -2,23 +2,24 @@
 
 import BlackboardQuiz
 
-with BlackboardQuiz.Quiz("Index notation questions") as quiz:
-    quiz.addQuestion('What is the following in index notation?<br/> $\\vec{a}+\\vec{b}$',
-                     ['The correct answer is $a_k+b_k$', '$a_i+a_i$', '$a_i+b_j$'],
-                     correct=1)
-    #Note that correct answer here is from 1 to 3 (not zero indexed)
+with BlackboardQuiz.Package("MyBlackboardPackage") as package:
+    with package.createPool('Index notation questions') as pool:
+        pool.addQuestion('What is the following in index notation?<br/> $\\vec{a}+\\vec{b}$',
+                         ['The correct answer is $a_k+b_k$', '$a_i+a_i$', '$a_i+b_j$'],
+                         correct=1)
+        #Note that correct answer here is from 1 to 3 (not zero indexed)
 
-    #You can also set the feedback using positive_feedback and
-    #negative_feedback which can also contain images or latex
-    quiz.addQuestion('This is a display equation: $$\\int x\,dx=?$$',
-                     ['$x$', '$\\frac{x^2}{2}$'],
-                     correct=2,
-                     positive_feedback="Well done!",
-                     negative_feedback="Sorry, but the rule for integration is $\\int x^n\\,dx=\\frac{x^{n+1}}{n+1}$ for $n\\neq -1$"
-    )
+        #You can also set the feedback using positive_feedback and
+        #negative_feedback which can also contain images or latex
+        pool.addQuestion('This is a display equation: $$\\int x\,dx=?$$',
+                         ['$x$', '$\\frac{x^2}{2}$', '$gh$'],
+                         correct=2,
+                         positive_feedback="Well done!",
+                         negative_feedback="Sorry, but the rule for integration is $\\int x^n\\,dx=\\frac{x^{n+1}}{n+1}$ for $n\\neq -1$"
+                     )
     
-    #Embedding external images is easy too
-    img_tag = quiz.embed_image('example_image.png')    
-    quiz.addQuestion('I cant believe that you can embed images!'+img_tag+' Cool huh?',
-                     ['Really cool', 'Well, its not that impressive, its basic functionality', 'Blackboard sucks'],
-                     correct=1)
+        #Embedding external images is easy too
+        pool.addQuestion('I cant believe that you can embed images! <img src="example_image.png"> Cool huh?',
+                         ['Really cool', 'Well, its not that impressive, its basic functionality', 'Blackboard sucks'],
+                         correct=1)
+        
