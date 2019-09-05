3c578715b8d1248b6bae02284cd81b6f594af3e8
Marcus Bannerman <m.bannerman@gmail.com>
2019-09-05 09:42:10 +0100

    Only works with python 2.7

--- BlackboardQuiz.py
+++ BlackboardQuiz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from lxml import etree
 import lxml.html as html
--- csv_parser.py
+++ csv_parser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 #An example way of parsing your own csv file format
 
